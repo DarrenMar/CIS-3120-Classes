830baf21bf83741f5424ae2668e25d67cd1a2764
DarrenMar <darrenmar2004@gmail.com>
2025-02-18 20:08:55 -0500

    Changed Animal class and Usage

--- Animal.py
+++ Animal.py
@@ -1,7 +1,23 @@
 class Animal:
-    def __init__(self): 
-        print("hello")
+    def __init__(self, name):
+        self.__name = name 
+        print("hello, I am", self.__name)
 
     def talk(self):
-        print("hi")
+        print(self.__name, "is talking.")
+
+    def walk(self):
+        print(self.__name, "is walking.")
+
+    def run(self):
+        print(self.__name, "is running.")
+
+    def jump(self):
+        print(self.__name, "is jumping.")
+
+    def eat(self):
+        print(self.__name, "is eating.")
+
+    def sleep(self):
+        print(self.__name, "is sleeping.")
 
